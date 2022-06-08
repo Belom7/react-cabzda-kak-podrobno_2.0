46579fb33b4f2b76b9cee505c822e941fa46fbfa
Belom7 <Still07pkm@gmail.com>
2022-06-08 14:45:55 +0300

    20 - React кабзда как подробно: навороченный useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,35 @@
+import {useMemo, useState} from "react";
+
+export default {
+    title: 'useState'
+}
+
+function generateData() {               /* какие то сложные вычисления из за которых все подвисает*/
+    console.log('generate data')
+    return 333
+}
+
+export const exemple = () => {
+    console.log('exemple')
+    // useState каждый раз будет брать инициализационное значение
+
+    // такие подходы лучше использовать когда идет какое то долгое вычисление
+
+    // const initialValue = generateData() /* при таком подходе каждый раз при клике на кнопку будет вызываться функция generateData и приложение будет подвисать*/
+    // const initialValue = useMemo(generateData,[]) /*для того чтоб функция не пере вызывалась обворачиваем ее в useMemo и делаем его с пустым массивом зависимостей(вызовет 1 раз)*/
+    // const [counter, setCounter] = useState(initialValue)
+
+    const [counter, setCounter] = useState(generateData) /*можно передать сразу функцию и она вызовется 1 раз*/
+
+    const changer = (state:number) => {       /*функция изменитель принимает параметр (состояние) которое сохранил в себе реакт и на его основе выдает нам новые данные */
+        return state + 1
+    }
+
+    return (
+        <div>
+            {counter}
+            {/*<button onClick={() => setCounter(counter + 1)}>klick</button>       /!*первый вариант*!/*/}
+            <button onClick={() => setCounter(changer)}>klick</button>       {/*второй вариант можем передать функцию*/}
+        </div>
+    )
+}
